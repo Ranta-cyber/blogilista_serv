ba0988c15cd4fcc8347d302e0f333d515773d008
Jorma Merivalssi <jormamerivalssi@Jormas-MacBook-Pro.local>
2020-10-05 09:48:17 +0300

    Rout

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose')
 
 const server = http.createServer(app)
 
+
 //const server = http.createServer((request, response) => {
 //  response.writeHead(200, { 'Content-Type': 'text/plain' })
 //  response.end('Hello World')
