30d34109955cafd4289c1d2d85840f32d9343bfa
sirpa <64065711+Ranta-cyber@users.noreply.github.com>
2020-11-05 19:09:26 +0200

    cypress lisäys

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -6,16 +6,14 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-  
+
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-
-
 blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
-  
+
   const decodedToken = jwt.verify(request.token, process.env.SECRET)
   //console.log('body:',request.body)
   console.log('token:',request.token)
@@ -27,15 +25,12 @@ blogsRouter.post('/', async (request, response) => {
     return
   }
   //const token = getTokenFrom(request)
-  
+
   if (!request.token || !decodedToken.id) {
     return response.status(401).json({ error: 'token missing or invalid' })
   }
 
   const user = await User.findById(decodedToken.id)
-
-  //const user = await User.findById(request.body.userId)
-  //console.log('user:', user)
   blog.user = user._id
 
   //console.log('blog.user:',blog)
@@ -51,12 +46,14 @@ blogsRouter.delete('/:id', async (request, response) => {
   response.status(204).end()
 })
 
-blogsRouter.put('/api/blogs/:id', async (request, response, next) => {
+blogsRouter.put('/:id', async (request, response, next) => {
   const body = request.body
 
-  
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  const user = await User.findById(decodedToken.id)
 
   const blogi = {
+    user: user._id,
     author: body.author,
     title: body.title,
     url: body.url,
